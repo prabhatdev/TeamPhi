a134134298672fb825c653769b76fce7fd9e1628
Prabhat <prabhat.singh374@gmail.com>
2018-08-17 02:14:23 +0530

    Retrofit Working.

--- app/src/main/java/com/example/prabh/teamphi/mvvm/activity/mainactivity/MainActivityViewModel.kt
+++ app/src/main/java/com/example/prabh/teamphi/mvvm/activity/mainactivity/MainActivityViewModel.kt
@@ -2,12 +2,32 @@ package com.example.prabh.teamphi.mvvm.activity.mainactivity
 
 import android.arch.lifecycle.MutableLiveData
 import android.arch.lifecycle.ViewModel
+import com.example.prabh.teamphi.retrofit.model.LoginResult
+import com.example.prabh.teamphi.utility.ApiType
 import com.example.prabh.teamphi.utility.Response
+import com.example.prabh.teamphi.utility.Utils
+import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.schedulers.Schedulers
 
 class MainActivityViewModel : ViewModel()
 {
     val compositeDisposable=CompositeDisposable()
     val response:MutableLiveData<Response> = MutableLiveData()
 
+    fun getData(userName:String,password:String)
+    {
+        val mApiService = Utils.interfaceService
+
+        compositeDisposable.add(mApiService.loginUser(userName, password)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .doOnSubscribe {
+                    response.value= Response.loading(ApiType.LOGIN_USER)
+                }
+                .subscribe(
+                        {it: LoginResult -> response.value = Response.success(ApiType.LOGIN_USER,it) },
+                        { throwable:Throwable -> response.value = Response.error(ApiType.LOGIN_USER,throwable) }
+                ))
+    }
 }
--- app/src/main/java/com/example/prabh/teamphi/utility/Utils.kt
+++ app/src/main/java/com/example/prabh/teamphi/utility/Utils.kt
@@ -2,29 +2,41 @@ package com.example.prabh.teamphi.utility
 
 import android.content.Context
 import android.widget.Toast
+import com.example.prabh.teamphi.retrofit.handler.ApiInterface
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import retrofit2.converter.gson.GsonConverterFactory
 
-class Utils(private val tag:String)
-{
+class Utils(private val tag: String) {
     companion object {
         private const val BASE_URL = ""
 
 
-        fun provideUtil(context: Context) : Utils{
+        fun provideUtil(context: Context): Utils {
             var activityname = Utils.getClassName(context.javaClass.name)
-            activityname=if (activityname.length>20) {
+            activityname = if (activityname.length > 20) {
                 activityname.substring(0, 20)
-            }
-            else{
+            } else {
                 activityname
             }
             return Utils(activityname)
         }
 
-        fun showToast(context: Context,message:String,length:Int) {
-            Toast.makeText(context,message,length).show()
+        fun showToast(context: Context, message: String, length: Int) {
+            Toast.makeText(context, message, length).show()
         }
 
         fun getClassName(fullName: String) = fullName.substring(fullName.lastIndexOf('.') + 1, fullName.length)
-    }
 
+        val interfaceService: ApiInterface
+            get() {
+                val BASE_URL = "http://13.126.180.89/Main/"
+                val retrofit = Retrofit.Builder()
+                        .baseUrl(BASE_URL)
+                        .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+                        .addConverterFactory(GsonConverterFactory.create())
+                        .build()
+                return retrofit.create<ApiInterface>(ApiInterface::class.java)
+            }
+    }
 }
